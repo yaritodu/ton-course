36e7cc9802f45883c90d4050795132fadf5af77e
Yaritodu <yaroslav985@icloud.com>
2024-06-02 16:28:51 +0300

    app.tsx add an extra

--- counter-front-end/src/App.tsx
+++ counter-front-end/src/App.tsx
@@ -2,17 +2,20 @@ import "./App.css";
 import { TonConnectButton } from "@tonconnect/ui-react";
 import { useMainContract } from "./hooks/useMainContract";
 import { useTonConnect } from "./hooks/useTonConnect";
+import { fromNano } from "@ton/core";
 
 function App() {
   const {
     contract_address,
     counter_value,
     contract_balance,
+    sendIncrement,
+    sendDeposit,
+    sendWithdrawalRequest,
   } = useMainContract();
 
-  const { sendIncrement } = useMainContract();
-  const { connected } = useTonConnect()
-  
+  const { connected } = useTonConnect();
+
   return (
     <div>
       <div>
@@ -23,26 +26,47 @@ function App() {
           <b>Our contract Address</b>
           <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
           <b>Our contract Balance</b>
-          <div className='Hint'>{contract_balance}</div>
+          {contract_balance && <div className='Hint'>{fromNano(contract_balance)}</div>}
         </div>
 
         <div className='Card'>
           <b>Counter Value</b>
           <div>{counter_value ?? "Loading..."}</div>
-          
-          {
-            connected && (
-              <a
-                onClick={() => {
-                  sendIncrement();
-                }}
-              >
-                Increment by 1
-              </a>
-            )
-          }
-
         </div>
+        {connected && (
+          <a
+            onClick={() => {
+              sendIncrement();
+            }}
+          >
+            Increment by 1
+          </a>
+        )}
+
+        <br/>
+
+        {connected && (
+          <a
+            onClick={() => {
+              sendDeposit();
+            }}
+          >
+            Request deposit of 1 ton
+          </a>
+        )}
+
+        <br/>
+
+        {connected && (
+          <a
+            onClick={() => {
+              sendWithdrawalRequest();
+            }}
+          >
+            Request 0.7 TON withdrawal
+          </a>
+        )}
+
       </div>
     </div>
   );
