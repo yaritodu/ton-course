3ea60bd592fe26ccb9291ca51e55990a76e1fe91
Yaritodu <yaroslav985@icloud.com>
2024-06-02 18:01:24 +0300

    Workflow

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://raw.githubusercontent.com/yaritodu/ton-course/main/public/tonconnect-manifest.json';
+const manifestUrl = 'https://github.com/yaritodu/ton-course/blob/main/public/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
