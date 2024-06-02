4773e3adbab70598e924d2e69fbf5b9a30724696
Yaritodu <yaroslav985@icloud.com>
2024-06-02 17:05:07 +0300

    fe deploy

--- counter_front_end/src/main.tsx
+++ counter_front_end/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+const manifestUrl = 'https://yaritodu.github.io/ton-course/counter_front_end/public/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
